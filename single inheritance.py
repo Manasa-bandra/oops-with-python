8798860de260a59f6dff7f51c12aa31b13163a0a
ManasaBandra <25ra1a66c3@kpritech.ac.in>
2026-09-07 16:14:49 +0530

    Add files via upload

---
+++ single inheritance.py
@@ -0,0 +1,13 @@
+class Demo:
+    Branch = "AIML"
+    def addition(self, a, b):
+        print(a + b)
+
+    def subtraction(self, a, b):
+        print(a - b)
+    def multiply(self, a, b):
+        print(a * b)
+d1 = Demo()
+d1.addition(10, 20)      
+d1.subtraction(30, 10)   
+d1.multiply(2, 10)       
